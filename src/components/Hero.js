d209d1f76b2930b632d727daae55572aa1715d3d
Andrea McDougald <andrea0nly@gmail.com>
2022-09-27 10:04:18 -0600

    hero copy

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -19,15 +19,15 @@ export default function Hero() {
         <div className="mx-auto w-full max-w-7xl pt-16 pb-20 text-center lg:pb-[10rem] lg:pt-24 lg:text-left">
           <div className="px-4 sm:px-8 lg:w-1/2 xl:pr-16">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
-              <span className="block xl:inline">Lorem ipsum write with</span>{" "}
+              <span className="block xl:inline">
+                Practise proven healing methods with
+              </span>{" "}
               <span className="inline text-sage">Writemind</span>
             </h1>
             <p className="mx-auto mt-3 max-w-md text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-              fugiat aliqua.
+              Expert-backed writing exercises to help you feel your best.
             </p>
-<hr className="block lg:hidden py-5 mt-9"/>
+            <hr className="block lg:hidden py-5 mt-9" />
             <div className="grid w-full h-8 grid-cols-[1fr_1fr_fr_1fr] lg:grid-cols-1">
               <div></div>
               <div className="min-w-max col-span-2 mt-7">
